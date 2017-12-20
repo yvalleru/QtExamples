e10f34e928451767bfd3c98d228a9f25a39f9ea1
unknown <yv@SOFT-LB.iseg.local>
2017-12-20 12:46:33 +0100

    QList: Start example 1
    Change-Id: I3d7123bff5522f86855801563feb4e117c020763

---
+++ mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_add_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QList<int> m_list;
+};
+
+#endif // MAINWINDOW_H
