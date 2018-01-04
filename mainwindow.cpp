53b59aa0985e74a27b90842264fbeea1bd8c997a
yv <valleruyuva@gmail.com>
2018-01-04 12:27:49 +0100

    Qlist.insert

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "qstandarditemmodel.h"
 
 #include <QDebug>
 
@@ -9,17 +10,15 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     initialisevalues();
-
+    m_list.append(2);
+    m_list.append(3);
+    m_list.append(5);
+    printValues();
 }
 
 void MainWindow::initialisevalues()
 {
-
-    m_str << "VoltageSet" << "CurrentSet"<< "VoltageNominal" << "CurrentNominal" << "ModuleList"
-        << "PowerOn" << "Article" << "ServerVersion" << "CrateNumber" << "Temperature";
-    ui->listWidget->addItems(m_str);
-    for(int i=0;i<10; ++i)
-        m_list.push_back(i);
+    m_list.reserve(0);
 }
 
 MainWindow::~MainWindow()
@@ -29,28 +28,44 @@ MainWindow::~MainWindow()
 
 void MainWindow::printValues()
 {
-    ui->textedit->clear();
-    for(int i=0; i<m_list.size(); ++i) {
-        ui->textedit->append(QString::number(i));
+    ui->log->append(tr("New list contents:"));
+    QString line;
+    for(int i=0; i<m_list.size(); ++i)
+    {
+        line.append(QString::number(m_list.at(i)));
+        if (i < (m_list.size() - 1)) {
+            line.append(", ");
+        }
     }
+    ui->log->append("[ " + line + " ]");
 }
 
 void MainWindow::on_add_clicked()
 {
-
     m_list.push_back(m_list.size());
     printValues();
-
 }
 
 void MainWindow::on_remove_clicked()
 {
-    if(!m_list.empty())
-    {
-        m_list.pop_back();
-        printValues();
-    }
    qDeleteAll(ui->listWidget->selectedItems());
+   QString listl = ui->listWidget->selectedItems().front()->text();
+    m_list.removeAt(m_list.contains(listl.toInt()));
 
 }
 
+
+
+void MainWindow::on_insert_clicked()
+{
+    if ( (0 < (ui->index->value()) && (ui->index->value()) < (m_list.size()+1) )){
+
+        m_list.insert(ui->index->value(),ui->number->value());
+
+        ui->log->append(tr("Adding number %1 to list at position %2")
+                        .arg(ui->number->value()).arg(ui->index->value()));
+        printValues();
+    } else {
+        ui->log->append(tr("out of order"));
+    }
+}
